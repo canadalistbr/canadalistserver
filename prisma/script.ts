ef8000e2f8d50c055ef88474bf226565287b54bb
Pyx4 <leost3@github.com>
2023-05-22 19:43:13 -0400

    chore: fix script to get DATABASE_URL correctly

--- prisma/script.ts
+++ prisma/script.ts
@@ -1,19 +1,26 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
+import dotenv from "dotenv";
+dotenv.config(); // Load environment variables from .env file
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient({
+  datasources: {
+    db: {
+      url: process.env.DATABASE_URL,
+    },
+  },
+});
 
 async function main() {
-    const c = await prisma.provinces.findMany()
-    console.log(c);
-    
+  const c = await prisma.provinces.findMany();
+  console.log(c);
 }
 
 main()
   .then(async () => {
-    await prisma.$disconnect()
+    await prisma.$disconnect();
   })
   .catch(async (e) => {
-    console.error(e)
-    await prisma.$disconnect()
-    process.exit(1)
-  })
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
