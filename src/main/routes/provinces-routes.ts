3d7973ee9a76fa14ef0c4e28da6b52f63e1c2151
leo <lstudart@pyx4.com>
2023-05-24 20:16:22 -0400

    feat: fetch provinces in /provinces route

--- src/main/routes/provinces-routes.ts
+++ src/main/routes/provinces-routes.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express'
+import { routeAdapter } from '../adapters/express-route-adapter'
+import { makeProvinces } from '../factories/provinces/provinces-factory'
 
 export default (route: Router) => {
-  route.get('/provinces', (req, res) => {
-    res.json({ cat: 'kiko' })
-  })
+  route.get('/provinces',routeAdapter(makeProvinces()))
 }
