2ed471deac7fabef1341405b741d80f49955f452
leo <lstudart@pyx4.com>
2023-05-26 15:56:27 -0400

    feat: check-province-by-id usecase implementeation

---
+++ src/data/usecases/load-provinces/db-check-province-id.ts
@@ -0,0 +1,15 @@
+import { CheckProvinceById } from '../../../domain/usecases/check-province-by-id'
+import { FindProvinceRepository } from '../../protocols/find-province/find-province-repository'
+import { ProvinceModel } from './protocols'
+
+export class DbCheckProvinceById implements CheckProvinceById {
+  constructor(private readonly findProvince: FindProvinceRepository) {}
+  async check(id: number): Promise<boolean> {
+    const province = await this.findProvince.findProvinceById(id)
+    if (province === null) return false
+    return true
+  }
+  isEmpty(province: ProvinceModel) {
+    return Object.keys(province).length === 0
+  }
+}
