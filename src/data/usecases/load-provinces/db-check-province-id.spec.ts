c5945e1ad4f6fc494fcb75acc0b724fcccacba96
leo <lstudart@pyx4.com>
2023-05-26 15:56:35 -0400

    test: check-province-by-id usecase implementeation

---
+++ src/data/usecases/load-provinces/db-check-province-id.spec.ts
@@ -0,0 +1,57 @@
+import { FindProvinceRepository } from '../../protocols/find-province/find-province-repository'
+import { DbCheckProvinceById } from './db-check-province-id'
+import { ProvinceModel } from './protocols'
+const makeFakeProvinceFactory = (): ProvinceModel => {
+  return {
+    id: 1,
+    capital: 'quebec city',
+    flag_url: 'randomUrl',
+    image_url: 'randomUrl',
+    language: ['fr', 'en'],
+    immigration_ranking: 4,
+    name: 'Quebec',
+    slug: 'Qc',
+    top_cities: ['montreal']
+  }
+}
+
+type SutType = {
+  findProvinceRepositoryStub: FindProvinceRepository
+  sut: DbCheckProvinceById
+}
+
+const makeSut = (): SutType => {
+  class FindProvinceRepositoryStub implements FindProvinceRepository {
+    findProvinceById(id: number): Promise<ProvinceModel> {
+      return new Promise((resolve, reject) =>
+        resolve(makeFakeProvinceFactory())
+      )
+    }
+  }
+
+  const findProvinceRepositoryStub = new FindProvinceRepositoryStub()
+  const sut = new DbCheckProvinceById(findProvinceRepositoryStub)
+  return {
+    sut,
+    findProvinceRepositoryStub
+  }
+}
+
+describe('', () => {
+  it('calls repository', async () => {
+    const { sut, findProvinceRepositoryStub } = makeSut()
+    const find = jest.spyOn(findProvinceRepositoryStub, 'findProvinceById')
+    await sut.check(1)
+    expect(find).toHaveBeenCalled()
+  })
+  it('returns true if province exists', async () => {
+    const { sut } = makeSut()
+    const response = await sut.check(1)
+    expect(response).toBe(true)
+  })
+
+  it('returns false if province does not not exists', async () => {
+    const { sut } = makeSut()
+    await sut.check(1)
+  })
+})
