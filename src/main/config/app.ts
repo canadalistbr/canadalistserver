a446b95223173625fa8e9d670a8c7d2329694d19
Pyx4 <leost3@github.com>
2023-05-27 10:56:21 -0400

    refactor: have setuoRoutes before setupMiddelwares

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,7 +1,7 @@
-import express from 'express'
-import setupMiddlewares from './middlewares'
-import setupRoutes from './routes'
-const app = express()
-setupMiddlewares(app)
-setupRoutes(app)
-export default app
+import express from "express";
+import setupMiddlewares from "./middlewares";
+import setupRoutes from "./routes";
+const app = express();
+setupRoutes(app);
+setupMiddlewares(app);
+export default app;
