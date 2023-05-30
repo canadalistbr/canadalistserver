d3056a2bfdb0330fbf9497984c66fa5ce44e9108
Pyx4 <leost3@github.com>
2023-05-29 22:17:07 -0400

    feat: use body and params in controller in the express route adapter

--- src/main/adapters/express-route-adapter.ts
+++ src/main/adapters/express-route-adapter.ts
@@ -1,9 +1,13 @@
-import { Request, Response } from 'express'
-import { Controller } from '../../presentation/protocols'
+import { Request, Response } from "express";
+import { Controller } from "../../presentation/protocols";
 
 export function routeAdapter(controller: Controller) {
   return async (req: Request, res: Response) => {
-    const { statusCode, body } = await controller.handle(req)
-    return res.status(statusCode).json(body)
-  }
+    const request = {
+      ...req.body,
+      ...req.params,
+    };
+    const { statusCode, body } = await controller.handle(request);
+    return res.status(statusCode).json(body);
+  };
 }
