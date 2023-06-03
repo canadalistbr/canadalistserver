2bf75dd6ab70ce0013c035c4d886b2c83bb9cda6
Pyx4 <leost3@github.com>
2023-06-03 19:57:55 -0400

    feat: add routes to api/city/cityid

---
+++ src/main/routes/find-city-routes.test.ts
@@ -0,0 +1,10 @@
+import request from "supertest";
+import app from "../config/app";
+
+describe("/api/cities", () => {
+  it("returns a 200", async () => {
+    await request(app)
+      .get("/api/cities/1a04456b-eeb1-407f-a1a2-ade93c94dce4")
+      .expect(200);
+  });
+});
