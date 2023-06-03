48ec3e73a5e2eb4d5ceb6a6e6b9e950d5dae16aa
Pyx4 <leost3@github.com>
2023-06-03 15:47:09 -0400

    feat: add ProvincesPrismaRepository

---
+++ src/infra/db/prisma/cities-repository/load-cities-repository.ts
@@ -0,0 +1,10 @@
+import { City } from "@prisma/client";
+import { LoadCitiesRepository } from "../../../../data/protocols/load-cities/load-cities-repository";
+import { prisma } from "../prisma";
+
+export class CitiesPrismaRepository implements LoadCitiesRepository {
+  loadAll(): Promise<City[]> {
+    const cities = prisma.city.findMany();
+    return cities;
+  }
+}
