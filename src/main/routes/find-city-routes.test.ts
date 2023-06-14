8aa0f7119a5f18686c243ebb16fb1cf4905c873f
Pyx4 <leost3@github.com>
2023-06-14 14:31:25 -0400

    test: add testes of sanitization to routes

--- src/main/routes/find-city-routes.test.ts
+++ src/main/routes/find-city-routes.test.ts
@@ -4,5 +4,7 @@ import app from "../config/app";
 describe("/api/cities", () => {
   it("returns a 200", async () => {
     await request(app).get("/api/cities/montreal").expect(200);
+    await request(app).get("/api/cities/Montreal").expect(200);
+    await request(app).get("/api/cities/Fort McMurray").expect(200);
   });
 });
