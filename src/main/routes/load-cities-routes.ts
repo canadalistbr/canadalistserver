6f05f6ee2573c7d4d95111223f2dca40853e9291
Pyx4 <leost3@github.com>
2023-06-16 14:58:08 -0400

    chore: fix name to load cities factory

--- src/main/routes/load-cities-routes.ts
+++ src/main/routes/load-cities-routes.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { routeAdapter } from "../adapters/express-route-adapter";
-import { makeLoadCities } from "../factories/cities/load-provinces-facory";
+import { makeLoadCities } from "../factories/cities/load-cities-facory";
 
 export default (route: Router) => {
   route.get("/cities", routeAdapter(makeLoadCities()));
