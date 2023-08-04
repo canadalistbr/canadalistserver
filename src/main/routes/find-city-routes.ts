8f6684b74adbcbad9b839f39a68664e22760c6cb
Pyx4 <leost3@github.com>
2023-08-04 16:09:18 -0400

    chore: remove unused imports

--- src/main/routes/find-city-routes.ts
+++ src/main/routes/find-city-routes.ts
@@ -1,9 +1,4 @@
 import { Router } from "express";
-import { FindCityController } from "../../presentation/controllers/cities/find-city";
-import { DbFindCity } from "../../data/usecases/find-city/db-find-city";
-import { DbCheckCityById } from "../../data/usecases/load-provinces/db-check-city-id ";
-import { CitiesPrismaRepository } from "../../infra/db/prisma/cities-repository/load-cities-repository";
-import { CityPrismaRepository } from "../../infra/db/prisma/city-repository/find-city-repository";
 import { routeAdapter } from "../adapters/express-route-adapter";
 import { makeFindCity } from "../factories/cities/find-city-factory";
 
