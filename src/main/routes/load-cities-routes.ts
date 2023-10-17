27acfccc7a82c6685414b99584d160c3dc1af6be
Leo <lstudart@pyx4.com>
2023-10-17 19:25:06 -0400

    refactor: fix name of file find cities factory

--- src/main/routes/load-cities-routes.ts
+++ src/main/routes/load-cities-routes.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { routeAdapter } from "../adapters/express-route-adapter";
-import { makeLoadCities } from "../factories/cities/load-cities-facory";
+import { makeLoadCities } from "../factories/cities/load-cities-factory";
 
 export default (route: Router) => {
   route.get("/cities", routeAdapter(makeLoadCities()));
