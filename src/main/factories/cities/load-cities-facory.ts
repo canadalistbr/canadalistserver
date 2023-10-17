3b981f8b59fd74ed5875f31b2fc233b53e2cf5f0
Leo <lstudart@pyx4.com>
2023-10-17 19:22:05 -0400

    feat: add request typo to load cities controller

--- src/main/factories/cities/load-cities-facory.ts
+++ src/main/factories/cities/load-cities-facory.ts
@@ -2,10 +2,10 @@ import { DbLoadCities } from "../../../data/usecases/load-cities/db-load-cities"
 import { CityModel } from "../../../domain/models/cities";
 import { CitiesPrismaRepository } from "../../../infra/db/prisma/cities-repository/load-cities-repository";
 import { LoadCitiesController } from "../../../presentation/controllers/cities/load-cities";
-import { Controller, HttpRequest } from "../../../presentation/protocols";
+import { Controller } from "../../../presentation/protocols";
 import { SlugInsertionUtil } from "../../../utils/add-slug/add-slug";
 
-export const makeLoadCities = (): Controller<HttpRequest> => {
+export const makeLoadCities = (): Controller<LoadCitiesController.Request> => {
   const addSlug = new SlugInsertionUtil<CityModel>();
   const loadCitiesRepository = new CitiesPrismaRepository();
   const loadCitiesUsecase = new DbLoadCities(loadCitiesRepository);
--- src/presentation/controllers/cities/load-cities.ts
+++ src/presentation/controllers/cities/load-cities.ts
@@ -1,21 +1,30 @@
 import { CityModel } from "../../../domain/models/cities";
 import { LoadCities } from "../../../domain/usecases/load-cities";
 import { ok, serverError } from "../../helpers";
-import { Controller, HttpRequest, HttpResponse } from "../../protocols";
+import { Controller, HttpResponse } from "../../protocols";
 import { SlugInsertion } from "../../protocols/slug-insertion";
 
-export class LoadCitiesController implements Controller<HttpRequest> {
+export class LoadCitiesController implements Controller<LoadCitiesController.Request> {
   constructor(
     private readonly loadCities: LoadCities,
     private readonly addSlug: SlugInsertion<CityModel>
-  ) {}
-  async handle(request: HttpRequest): Promise<HttpResponse> {
+  ) { }
+  async handle(request: LoadCitiesController.Request): Promise<HttpResponse> {
     try {
-      const cities = await this.loadCities.load();
+      const cities = await this.loadCities.load(request);
       const citiesWithSlug = cities.map((city) => this.addSlug.add(city));
       return ok(citiesWithSlug);
     } catch (error) {
+      console.log(error)
       return serverError(error);
     }
+  }
+}
+
+export namespace LoadCitiesController {
+  export type Request = {
+    bikeFriendly?: string
+    nature?: string
+    festivals?: string
   }
 }
